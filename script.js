6373c5b245f5c3bd55258197305c15e42d6901dc
Conall O Maolain <conallomaolain1@gmail.com>
2024-04-11 15:05:59 +0200

    add bio

---
+++ script.js
@@ -0,0 +1,4 @@
+const headline = document.querySelector('[data-js="headline"]');
+const aboutParagraph = document.querySelector('[data-js="about-paragraph"]');
+
+headline.innerHTML = "Welcome to Thar Anseo";
